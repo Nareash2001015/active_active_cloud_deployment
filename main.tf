4910421e332bc34b245dad3121e2ef7cc42163b6
nareash <nareash20010150@gmail.com>
2024-03-26 11:53:55 +0530

    Added neccessary files for terraform and helm

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.62.1"
+    }
+  }
+  backend "azurerm" {
+    storage_account_name = var.storage_account_name
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+    access_key           = var.storage_key
+  }
+}
+
+resource "azurerm_resource_group" "aks-rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+  location            = var.location
+  resource_group_name = azurerm_resource_group.aks-rg.name
+  dns_prefix          = var.cluster_name
+
+  default_node_pool {
+    name                = "system"
+    node_count          = var.system_node_count
+    vm_size             = "Standard_DS2_v2"
+    type                = "VirtualMachineScaleSets"
+    zones               = [1, 2, 3]
+    enable_auto_scaling = false
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    load_balancer_sku = "standard"
+    network_plugin    = "kubenet"
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+resource_group_name  = "aks_tf_rg"
+location             = "CentralUS"
+cluster_name         = "__clusterName__"
+kubernetes_version   = "1.26.3"
+system_node_count    = 2
+storage_account_name = "__terraformstorageaccount__"
+storage_key          = "__storagekey__"
